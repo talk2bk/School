e38aa6c836a1d9b6b2ce08ab52e0730ecb89c11b
Benjamin Klunpaitoon <talk2bk@hotmail.com>
2015-03-11 20:10:20 -0700

    updates to enable animation

--- Spring2015/CS134/GameWindow/GameWindow/GameWindow.c
+++ Spring2015/CS134/GameWindow/GameWindow/GameWindow.c
@@ -6,19 +6,39 @@
 #include <stdio.h>
 #include "DrawUtils.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef enum {false,true} bool;
+GLuint textures[20];
+
+typedef struct AnimFrameDef{
+	int frameNum;
+	float frameTime;
+}AnimFrameDef;
+
+typedef struct AnimDef{
+	const char* name;
+	AnimFrameDef frames[20];
+	int numFrames;
+}AnimDef;
+
+typedef struct AnimData{
+	AnimDef* def;
+	int curFrame;
+	float timeToNextFrame;
+	bool isPlaying;
+}AnimData;
 
 typedef struct {
 	int x, y, w, h;
+	AnimData data;
 	GLuint image;
 }AABB;
 
 //change all tiles to this format
-typedef struct{
+typedef struct Character{
 	AABB bounds;
-	GLuint image;
-}Tile;
+	AnimData data;
+}Character;
 
 unsigned int randr(unsigned int min, unsigned int max)
 {
@@ -57,6 +77,45 @@ bool AABBIntersect(const AABB* box1, const AABB* box2)
 		return false;
 	}
 	return true;
+}
+
+void animSet(AnimData* anim, AnimDef* toPlay){
+	anim->def = toPlay;
+	anim->curFrame = 0;
+	anim->timeToNextFrame = toPlay->frames[0].frameTime;
+	anim->isPlaying = true;
+}
+
+void animDraw(AnimData* anim, int x, int y, int w, int h){
+	AnimDef* def = anim->def;
+	int curFrameNum = def->frames[anim->curFrame].frameNum;
+	GLuint tex = textures[curFrameNum];
+	glDrawSprite(tex, x, y, w, h);
+}
+
+void animReset(AnimData*anim){
+	animSet(anim, anim->def);
+}
+
+void animTick(AnimData* data, float dt){
+	if (!data->isPlaying){
+		return;
+	}
+	int numFrames = data->def->numFrames;
+	data->timeToNextFrame -= dt;
+	if (data->timeToNextFrame < 0){
+		++data->curFrame;
+		if (data->curFrame >= numFrames){
+			//end of animation, stop it
+			data->curFrame = numFrames - 1;
+			data->timeToNextFrame = 0;
+			data->isPlaying = false;
+		}
+		else{
+			AnimFrameDef* curFrame = &data->def->frames[data->curFrame];
+			data->timeToNextFrame += curFrame->frameTime;
+		}
+	}
 }
 
 int main(void)
@@ -121,6 +180,13 @@ int main(void)
 	items[0] = glTexImageTGAFile("mush.tga", NULL, NULL);
 	items[1] = glTexImageTGAFile("skull.tga", NULL, NULL);
 	items[2] = glTexImageTGAFile("slow.tga", NULL, NULL);
+
+	textures[0] = tiles[0];
+	textures[1] = tiles[1];
+	textures[2] = items[0];
+	textures[3] = items[1];
+	textures[4] = items[2];
+
 
 	AABB mush; int itemHeight = 16, itemWidth = 16; 
 	int itemX = randr(0, 100), itemY = randr(0, 100);
