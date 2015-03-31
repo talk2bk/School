7ac3b2412626fa8cebd4f851b9c2324e19d75cbe
Benjamin Klunpaitoon <talk2bk@hotmail.com>
2015-03-30 23:21:24 -0700

    rewrote most of the code
    need to figure out things in the ToDoList

--- Spring2015/CS134/GameWindow/GameWindow/GameWindow.c
+++ Spring2015/CS134/GameWindow/GameWindow/GameWindow.c
@@ -8,19 +8,25 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-GLuint textures[20];
+//array of textures for everything
+GLuint textures[30];
+int numItems = 3;
+//structs
 
+//holds frame number and how long that frame exists
 typedef struct AnimFrameDef{
 	int frameNum;
 	float frameTime;
 }AnimFrameDef;
 
+//animation name, copy of frames, number of frames in teh animation
 typedef struct AnimDef{
 	const char* name;
 	AnimFrameDef frames[20];
 	int numFrames;
 }AnimDef;
 
+//takes the def, takes current frame, uses time to next frame and checks if the animation is playing.
 typedef struct AnimData{
 	AnimDef* def;
 	int curFrame;
@@ -28,36 +34,88 @@ typedef struct AnimData{
 	bool isPlaying;
 }AnimData;
 
+//bounding box
 typedef struct AABB{
 	int x, y, w, h;
-	AnimData data;
-	GLuint image;
 }AABB;
 
-//change all tiles to this format
-typedef struct Character{
+typedef struct Camera{
+	AABB bounds;
+}Camera;
+
+//player sprite, has a bounding box and animation data.
+typedef struct Player{
 	AABB bounds;
 	AnimData data;
-}Character;
+}Player;
 
-unsigned int randr(unsigned int min, unsigned int max)
-{
-	bool notInRange = true;
-	int result;
-	while (notInRange){
-		double scaled = (double)rand() / RAND_MAX;
+//any of the three items, mush, skull, or evilmush.
+typedef struct Item{
+	AABB bounds;
+	AnimData data;
+	bool collided;
+}Item;
 
-		result = ((max - min + 1)*scaled + min);
+typedef struct Tile{
+	int image;
+	AABB bounds;
+}Tile;
 
-		if (result <= max && result >= min)
-		{
-			notInRange = false;
-		}
-	}
+Tile background[40][40];
+Player player;
+Camera camera;
+Item mush;
+Item skull;
+Item slow;
+//number of items is 3.
+Item items[2];
 
-	return result;
+//methods
+
+//set which animation is running
+void animSet(AnimData* anim, AnimDef* toPlay){
+	anim->def = toPlay;
+	anim->curFrame = 0;
+	anim->timeToNextFrame = toPlay->frames[0].frameTime;
+	anim->isPlaying = true;
 }
 
+//draw the animation
+void animDraw(AnimData* anim, int x, int y, int w, int h){
+	AnimDef* def = anim->def;
+	int curFrameNum = def->frames[anim->curFrame].frameNum;
+	GLuint tex = textures[curFrameNum];
+	glDrawSprite(tex, x, y, w, h);
+}
+
+//reset the animation
+void animReset(AnimData*anim){
+	animSet(anim, anim->def);
+}
+
+//advance to next frame
+void animTick(AnimData* data, float dt){
+	if (!data->isPlaying){
+		return;
+	}
+	int numFrames = data->def->numFrames;
+	data->timeToNextFrame -= dt;
+	if (data->timeToNextFrame < 0){
+		++data->curFrame;
+		if (data->curFrame >= numFrames){
+			//end of animation, stop it
+			data->curFrame = numFrames - 1;
+			data->timeToNextFrame = 0;
+			data->isPlaying = false;
+		}
+		else{
+			AnimFrameDef* curFrame = &data->def->frames[data->curFrame];
+			data->timeToNextFrame += curFrame->frameTime;
+		}
+	}
+}
+
+//check intersection of AABB
 bool AABBIntersect(const AABB* box1, const AABB* box2)
 {
 	// box1 to the right
@@ -79,43 +137,28 @@ bool AABBIntersect(const AABB* box1, const AABB* box2)
 	return true;
 }
 
-void animSet(AnimData* anim, AnimDef* toPlay){
-	anim->def = toPlay;
-	anim->curFrame = 0;
-	anim->timeToNextFrame = toPlay->frames[0].frameTime;
-	anim->isPlaying = true;
-}
+unsigned int randr(unsigned int min, unsigned int max)
+{
+	bool notInRange = true;
+	int result;
+	while (notInRange){
+		double scaled = (double)rand() / RAND_MAX;
 
-void animDraw(AnimData* anim, int x, int y, int w, int h){
-	AnimDef* def = anim->def;
-	int curFrameNum = def->frames[anim->curFrame].frameNum;
-	GLuint tex = textures[curFrameNum];
-	glDrawSprite(tex, x, y, w, h);
-}
+		result = ((max - min + 1)*scaled + min);
 
-void animReset(AnimData*anim){
-	animSet(anim, anim->def);
-}
-
-void animTick(AnimData* data, float dt){
-	if (!data->isPlaying){
-		return;
-	}
-	int numFrames = data->def->numFrames;
-	data->timeToNextFrame -= dt;
-	if (data->timeToNextFrame < 0){
-		++data->curFrame;
-		if (data->curFrame >= numFrames){
-			//end of animation, stop it
-			data->curFrame = numFrames - 1;
-			data->timeToNextFrame = 0;
-			data->isPlaying = false;
-		}
-		else{
-			AnimFrameDef* curFrame = &data->def->frames[data->curFrame];
-			data->timeToNextFrame += curFrame->frameTime;
+		if (result <= max && result >= min)
+		{
+			notInRange = false;
 		}
 	}
+
+	return result;
+}
+
+void resetPos(Item *item){
+	item->bounds.x = randr(0, 600);
+	item->bounds.y = randr(0, 400);
+	item->collided = false;
 }
 
 int main(void)
@@ -169,72 +212,67 @@ int main(void)
 	/* Keep a pointer to SDL's internal keyboard state */
 	kbState = SDL_GetKeyboardState(NULL);
 	//*********************************************************************************************************
-
-	//tiles for world 16x16
-	int tileHeight = 16;
-	int tileWidth = 16;
-	GLuint tiles[1];
-	tiles[0] = glTexImageTGAFile("enderStone.tga", NULL, NULL);
-	tiles[1] = glTexImageTGAFile("obsidian.tga", NULL, NULL);
-	GLuint items[2];
-	items[0] = glTexImageTGAFile("mush.tga", NULL, NULL);
-	items[1] = glTexImageTGAFile("skull.tga", NULL, NULL);
-	items[2] = glTexImageTGAFile("slow.tga", NULL, NULL);
-
-	textures[0] = tiles[0];
-	textures[1] = tiles[1];
-	textures[2] = items[0];
-	textures[3] = items[1];
-	textures[4] = items[2];
-
-
-	AABB mush; int itemHeight = 16, itemWidth = 16;
-	int itemX = randr(0, 100), itemY = randr(0, 100);
-	mush.x = randr(100, 400); mush.y = randr(100, 400);
-	mush.h = itemHeight; mush.w = itemWidth;
-	mush.image = glTexImageTGAFile("mush.tga", NULL, NULL);
-	bool collided = false;
-
-	//character animations
-	AABB character;
 	int charHeight = 16;
 	int charWidth = 32;
-	int charX = 0;
-	int charY = 0;
-	character.x = charX; 	character.y = charY;
-	character.h = charHeight; 	character.w = charWidth;
-	character.image = glTexImageTGAFile("idleAnim.tga", &charWidth, &charHeight);
+	int tileHeight = 16; 
+	int tileWidth = 16;
+	textures[0] = glTexImageTGAFile("enderStone.tga", &tileWidth,&tileHeight);
+	textures[1] = glTexImageTGAFile("obsidian.tga", &tileWidth, &tileHeight);
+	textures[2] = glTexImageTGAFile("mush.tga", &tileWidth, &tileHeight);
+	textures[3] = glTexImageTGAFile("skull.tga", &tileWidth, &tileHeight);
+	textures[4] = glTexImageTGAFile("slow.tga", &tileWidth, &tileHeight);
 
-	//screen information
-	int screenWidth = 640;
-	int screenCenterWidth = screenWidth / 2;
-	int screenHeight = 480;
-	int screenCenterHeight = screenHeight / 2;
-	AABB camera;
-	int camX = 0; int camY = 0;
-	camera.x = camX; camera.y = camY, camera.w = screenWidth, camera.h = screenHeight;
+	textures[5] = glTexImageTGAFile("idleAnim.tga", &charWidth, &charHeight);
+	textures[6] = glTexImageTGAFile("walkOne.tga", &charWidth, &charHeight);
+	textures[7] = glTexImageTGAFile("walkTwo.tga", &charWidth, &charHeight);
 
+	items[0] = mush;
+	items[1] = skull;
+	items[2] = slow;
 
-	//change world array to work off of Tile structs, makes it easier for overlaying items
-	AABB worldArray[40][40];
+	//player
+	player.bounds.x = 0;
+	player.bounds.y = 0;
+	player.bounds.h = charHeight;
+	player.bounds.w = charWidth;
+	/*to do: set up animation stuff*/
+
+	//camera
+	camera.bounds.x = 0; 
+	camera.bounds.y = 0;
+	camera.bounds.w = 640;
+	camera.bounds.h = 480;
+
+	for (int i = 0; i < 3; i++){
+		resetPos(&items[i]);
+	}
+	/*to do: set up mush animation stuff*/
+	
+	/*to do: set up skull animation stuff*/
+	
+	/*to do: set up slow animation stuff*/
+
 	for (int i = 0; i <= 40; i++){
 		for (int j = 0; j <= 40; j++){
-			worldArray[i][j].w = tileWidth; worldArray[i][j].h = tileHeight;
-			worldArray[i][j].x = i * 16; worldArray[i][j].y = j * 16;
+			background[i][j].bounds.w = tileWidth;
+			background[i][j].bounds.h = tileHeight;
+			background[i][j].bounds.x = i * tileWidth;
+			background[i][j].bounds.y = j * tileHeight;
 
-			if (i % 2 == 0 || j % 2 == 0){
-				worldArray[i][j].image = tiles[0];
+			if ((i * 16) % 2 == 0 || (j * 16) % 2 == 0){
+				background[i][j].image = textures[0];
 			}
-			else {
-				worldArray[i][j].image = tiles[1];
+			else{
+				background[i][j].image = textures[1];
 			}
-
 		}
 	}
-	//max world size is 640x640
 
 	Uint32 lastFrameMs;
 	Uint32 currentFrameMs = SDL_GetTicks();
+	float physicsDeltaTime = 1 / 100.0f;
+	int physicsDeltaMs = 10;
+	Uint32 lastPhysicsFrameMs;
 
 	/* The game loop */
 	while (!shouldExit) {
@@ -255,9 +293,6 @@ int main(void)
 		//get keyboard state
 		kbState = SDL_GetKeyboardState(NULL);
 
-		glClearColor(0, 0, 0, 1);
-		glClear(GL_COLOR_BUFFER_BIT);
-
 		currentFrameMs = SDL_GetTicks();
 		//use for time since last frame
 		float deltaTime = (currentFrameMs - lastFrameMs) / 1000.0f;
@@ -265,75 +300,70 @@ int main(void)
 		//if esc quit game
 		if (kbState[SDL_SCANCODE_ESCAPE]){ shouldExit = true; }
 
-		//character controls
-		if (!kbState[SDL_SCANCODE_LEFT] && kbPrevState[SDL_SCANCODE_LEFT]){
-			if (character.x > 0) character.x -= 16;
-		}
-		else if (!kbState[SDL_SCANCODE_RIGHT] && kbPrevState[SDL_SCANCODE_RIGHT]){
-			if (character.x < 640 - tileWidth) character.x += 16;
-		}
-		if (!kbState[SDL_SCANCODE_UP] && kbPrevState[SDL_SCANCODE_UP]){
-			if (character.y > 0) character.y -= 16;
-		}
-		else if (!kbState[SDL_SCANCODE_DOWN] && kbPrevState[SDL_SCANCODE_DOWN]){
-			if (character.y < 480 - tileHeight * 2)character.y += 16;
-		}
-		//character controls end
-
-		//camera controls
-		if (kbState[SDL_SCANCODE_A]){
-			if (mush.x > 0) mush.x -= 8;
-		}
-		else if (kbState[SDL_SCANCODE_D]){
-			if (mush.x < 640 - tileWidth) mush.x += 8;
-		}
-		if (kbState[SDL_SCANCODE_W]){
-			if (mush.y > 0) mush.y -= 8;
-		}
-		else if (kbState[SDL_SCANCODE_S]){
-			if (mush.y < 480 - tileHeight * 2)mush.y += 8;
-		}
-		//camera controls end
-
-		//mush controls
-		if (kbState[SDL_SCANCODE_J]){
-			if (camera.x > 0) camera.x -= 8;
-		}
-		else if (kbState[SDL_SCANCODE_L]){
-			if (camera.x < 640 - tileWidth) camera.x += 8;
-		}
-		if (kbState[SDL_SCANCODE_I]){
-			if (camera.y > 0) camera.y -= 8;
-
-		}
-		else if (kbState[SDL_SCANCODE_K]){
-			if (camera.y < 480 - tileHeight * 2)camera.y += 8;
-		}
-		//mush controls end
-
-
+		//update and draw game
 		for (int i = 0; i <= 40; i++){
 			for (int j = 0; j <= 40; j++){
-				if (AABBIntersect(&camera, &worldArray[i][j])){
-					glDrawSprite(worldArray[i][j].image, worldArray[i][j].x - camera.x, worldArray[i][j].y - camera.y, tileHeight, tileWidth);
+				if (AABBIntersect(&camera, &background[i][j].bounds)){
+					glDrawSprite(background[i][j].image, background[i][j].bounds.x - camera.bounds.x, background[i][j].bounds.y - camera.bounds.y, background[i][j].bounds.h, background[i][j].bounds.w);
 				}
 
 			}
 
 		}
-		if (AABBIntersect(&character, &mush)){ collided = true; }
-		if (collided){
-			mush.x = randr(0, 600); mush.y = randr(0, 400); collided = false;
-			switch (randr(0, 1)){
-			case 0: mush.image = items[1]; break;
-			case 1: mush.image = items[2]; break;
-			default: mush.image = items[0]; break;
-			}
-		}
-		glDrawSprite(mush.image, mush.x, mush.y, mush.w, mush.h);
-		
-		animDraw(&character.data, character.x, character.y, character.w, character.h);
 
+		//update player based on input. playerUpdate(&player, deltaTime);
+		if (kbState[SDL_SCANCODE_LEFT] && !kbPrevState[SDL_SCANCODE_LEFT]){
+			if (player.bounds.x > 0) player.bounds.x -= 16;
+		}
+		else if (kbState[SDL_SCANCODE_RIGHT] && !kbPrevState[SDL_SCANCODE_RIGHT]){
+			if (player.bounds.x < 640 - tileWidth) player.bounds.x += 16;
+		}
+		if (kbState[SDL_SCANCODE_UP] && !kbPrevState[SDL_SCANCODE_UP]){
+			if (player.bounds.y > 0) player.bounds.y -= 16;
+		}
+		else if (kbState[SDL_SCANCODE_DOWN] && !kbPrevState[SDL_SCANCODE_DOWN]){
+			if (player.bounds.y < 480 - tileHeight * 2) player.bounds.y += 16;
+		}
+		//update camera, items based on input and player. cameraUpdate(&camera, deltaTime); for(int i = 0; i < numitems; ++i){itemUpdate(&items[i],deltaTime);}
+		if (kbState[SDL_SCANCODE_A]){
+			if (camera.bounds.x > 0) camera.bounds.x -= 8;
+		}
+		else if (kbState[SDL_SCANCODE_D]){
+			if (camera.bounds.x < 640 - tileWidth) camera.bounds.x += 8;
+		}
+		if (kbState[SDL_SCANCODE_W]){
+			if (camera.bounds.y > 0) camera.bounds.y -= 8;
+		}
+		else if (kbState[SDL_SCANCODE_S]){
+			if (camera.bounds.y < 480 - tileHeight * 2) camera.bounds.y += 8;
+		}
+
+		if (AABBIntersect(&player.bounds, &mush.bounds)){ mush.collided = true; }
+		if (AABBIntersect(&player.bounds, &skull.bounds)){ skull.collided = true; }
+		if (AABBIntersect(&player.bounds, &slow.bounds)){ slow.collided = true; }
+		if (mush.collided){ resetPos(&mush); }
+		if (skull.collided){ resetPos(&skull); }
+		if (slow.collided){ resetPos(&slow); }
+
+		//physics stuff
+		//do{
+			//physics movement
+			//physics collision detection
+			//physics collision resolution
+			//lastPhysicsFrameMs += physicsDeltaMs;
+		//} while(lastPhysicsFrameMs + physicsDeltaMs < curFrameMs);
+
+		//draw the current state of everything. playerDraw(&player);
+		/*to do: draw items, then draw character.*/
+		/*
+		for(int i = 0; i < 3; i++){
+		animDraw(&items[i], items[i].bounds.x, items[i].bounds.y, items[i].bounds.w, items[i].bounds.h);
+		*/
+		//animDraw(&player, player.bounds.x, player.bounds.y, player.bounds.w, player,bounds.h);
+
+
+		glClearColor(0, 0, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
 		SDL_GL_SwapWindow(window);
 	}
 	SDL_Quit();
